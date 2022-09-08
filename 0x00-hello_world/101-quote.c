4a8249c33db0da86fe15c4598c2e06c732ed3fbc
ENGRSMYLE <101359280+ENGRSMYLE@users.noreply.github.com>
2022-09-08 09:24:09 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,23 @@
-#include <stdio.h>
-#include <unistd.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-return (1);
-}
+#include <stdio.h> 
+ #include <string.h> 
+ #include <unistd.h> 
+ /** 
+  * main - main block 
+  * Return: 1 
+  */ 
+ int main(void) 
+ { 
+         int s; 
+  
+         s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"); 
+  
+         write(2, 
+               "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
+               s); 
+         return (1); 
+ }
+
+
+
+
+
