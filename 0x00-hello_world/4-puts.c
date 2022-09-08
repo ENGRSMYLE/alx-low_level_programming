37313c3cbbbeddbc108c946508ba841fb50dfbc5
ENGRSMYLE <101359280+ENGRSMYLE@users.noreply.github.com>
2022-09-08 09:00:37 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 /**
  * Description: main - Prints out a string to stdout.
- * Return: 0 if correct.
+ * Return: 0 if success.
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return
+return (0);
+}
+
+
+
+
+
+
+
+
