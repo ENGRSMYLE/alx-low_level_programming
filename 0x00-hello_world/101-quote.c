8165f2d7a855feb07d9fd6d821699b0afcf1ce69
ENGRSMYLE <101359280+ENGRSMYLE@users.noreply.github.com>
2022-09-08 17:30:06 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,35 +1,4 @@
 #include <stdio.h> 
-<<<<<<< HEAD
-
-#include <unistd.h> 
-
-
-
-/**
- *
- *  * main - print quote 
- *
- *   *
- *
- *    * Description: prints Dors's quote 
- *
- *     *
- *
- *      * Return: Always 1 (Success) 
- *
- *       */
-
-
-
-int main(void) 
-
-{
-
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); 
-
-			return (1); 
-
-=======
 #include <unistd.h> 
 
 /**
@@ -44,5 +13,4 @@ int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); 
 	return (1); 
->>>>>>> 9f8bc20e2856f01ce8e1c0bdbf0ee7e658c6ec46
 } 
